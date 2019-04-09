a4b62d7dccb04d98a29b667b9d2a18879aedc92b
zehanhopkins <49429814+zehanhopkins@users.noreply.github.com>
2019-04-09 00:43:26 -0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,12 @@
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
